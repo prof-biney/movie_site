13a4a913021aff6c256d4fea1521ca7c932ee2e0
abiney1321 <abiney1321@gmail.com>
2025-03-21 16:23:36 +0000

    set up header

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,20 @@
 export default function App() {
-  return <main>Hello</main>;
+  return (
+    <main>
+      <div className="pattern" />
+
+      <div className="wrapper">
+        <header>
+          <img src="./hero.png" alt="Hero Banner" />
+
+          <h1>
+            Find <span className="text-gradient">Movies</span> You'll Enjoy
+            Without the Hassle
+          </h1>
+        </header>
+
+        <p>Search</p>
+      </div>
+    </main>
+  );
 }
